679821d738189306a4e75040b25156a64806be23
DouglasDG <114673280+DouglasDGA@users.noreply.github.com>
2022-11-04 14:02:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+var $h1 = document.getElementById("Subtitulo");
+var $div = document.getElementById("Subtitulo");
+console.log($div.firstChild);
+console.log($h1.textContent);
+console.log($h1);
+$div = document.getElementById("Subtitulo");
+//alert(div)
+$div.innerHTML = "Principal";
+
+
+var $h1 = document.getElementById("Subtitulo-2");
+var $div = document.getElementById("Subtitulo-2");
+console.log($div.firstChild);
+console.log($h1.textContent);
+console.log($h1);
+$div = document.getElementById("Subtitulo-2");
+//alert(div)
+$div.innerHTML = "Empresa";    
+
+
+var $h1 = document.getElementById("Subtitulo-3");
+var $div = document.getElementById("Subtitulo-3");
+console.log($div.firstChild);
+console.log($h1.textContent);
+console.log($h1);
+$div = document.getElementById("Subtitulo-3");
+//alert(div)
+$div.innerHTML = "Serviços";
+
+
+var $h1 = document.getElementById("Subtitulo-4");
+var $div = document.getElementById("Subtitulo-4");
+console.log($div.firstChild);
+console.log($h1.textContent);
+console.log($h1);
+$div = document.getElementById("Subtitulo-4");
+//alert(div)
+$div.innerHTML = "Contato";
+
+
+var $h1 = document.getElementById("Subtitulo-5");
+var $div = document.getElementById("Subtitulo-5");
+console.log($div.firstChild);
+console.log($h1.textContent);
+console.log($h1);
+$div = document.getElementById("Subtitulo-5");
+//alert(div)
+$div.innerHTML = "Novidades";
+
+
+var $h1 = document.getElementById("Subtitulo-6");
+var $div = document.getElementById("Subtitulo-6");
+console.log($div.firstChild);
+console.log($h1.textContent);
+console.log($h1);
+$div = document.getElementById("Subtitulo-6");
+//alert(div)
+$div.innerHTML = "Inscrição ou Login";
+
+
+var $h1 = document.getElementById("Informativo");
+var $div = document.getElementById("Informativo");
+console.log($div.firstChild);
+console.log($h1.textContent);
+console.log($h1);
+$div = document.getElementById("Informativo");
+//alert(div)
+$div.innerHTML = "Informativo";
+
